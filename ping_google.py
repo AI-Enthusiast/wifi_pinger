abd701669610f73ef5d62bd773730c32acb5517b
cormac <cormacdacker@gmail.com>
2025-01-12 22:55:06 -0800

    removed: sound alert

--- ping_google.py
+++ ping_google.py
@@ -1,22 +1,19 @@
 import datetime
 import time
 import requests
-from playsound import playsound
 
 def ping_google(connected=True, lost_pings=0, alert_played=False):
     try:
         requests.get('http://www.google.com', timeout=5)
         if not connected:
             print("Internet connection re-established at\t\t\t", datetime.datetime.now(), '\n')
-            playsound('alert_sound.mp3')
         connected = True
         time.sleep(20)
         ping_google(connected)
     except requests.ConnectionError:
         if connected:
             print("Lost internet connection, attempting to reconnect at", datetime.datetime.now())
-        if lost_pings > 1 and not alert_played:
-            playsound('alert_sound.mp3')
+
             alert_played = True
         connected = False
         time.sleep(10)
